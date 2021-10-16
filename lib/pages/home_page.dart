dba16870877b1507f0c75ad9afe88af4d0d68ced
tas-shakil <tas.shakil@gmail.com>
2021-10-16 15:39:55 +0600

    day13

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -46,13 +46,50 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty) ? ListView.builder(
-            itemCount: CatalogModel.items.length,
-            itemBuilder: (context,index) {
-              return ItemWidget(
-                  item: CatalogModel.items[index]);
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty) ?
+         GridView.builder(
+             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                 crossAxisCount: 2,
+                 mainAxisSpacing: 16,
+                 crossAxisSpacing: 16),
+             itemBuilder: (context, index){
+               final item = CatalogModel.items[index];
+               return Card(
+                 clipBehavior: Clip.antiAlias,
+                 shape: RoundedRectangleBorder(
+                     borderRadius: BorderRadius.circular(10)),
+                   child: GridTile(
+                       header: Container(
+                           child: Center(
+                             child: Text(item.name,style: const TextStyle(
+                               color: Colors.white,
+                             ),
+                             ),
+                           ),
+                           padding: const EdgeInsets.all(12),
+                           decoration: const BoxDecoration(
+                             color: Colors.deepPurple,
 
-        }): const Center(
+                           ),
+
+                       ),
+                       child: Image.network(item.image),
+                       footer: Container(
+                           child: Text(item.price.toString(), style: const TextStyle(
+                             color: Colors.white
+                           ),
+                           ),
+                           padding: const EdgeInsets.all(12),
+                          decoration: const BoxDecoration(
+                            color: Colors.black
+                          ),
+                       ),
+                   ),
+               );
+             },
+             itemCount: CatalogModel.items.length,
+         )
+            : const Center(
           child: CircularProgressIndicator(),
         ),
       ),
